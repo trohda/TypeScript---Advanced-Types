adccd252c755314d06ece313721b7ae4f8d59031
Tomasz Rohda <tomaszrohda@wp.pl>
2022-08-01 10:56:48 +0200

    Type Casting

--- src/app.ts
+++ src/app.ts
@@ -110,3 +110,19 @@ function moveAnimal(animal: Animal) {
 }
 
 moveAnimal({ type: "bird", flyingSpeed: 10 });
+
+// TYPE CASTING
+// const userInputElement = <HTMLInputElement>(
+//   document.getElementById("user-input")!
+// );
+
+// const userInputElement = document.getElementById(
+//   "user-input"
+// )! as HTMLInputElement;
+// userInputElement.value = "Hi there!";
+
+const userInputElement = document.getElementById("user-input");
+
+if (userInputElement) {
+  (userInputElement as HTMLInputElement).value = "Hi there!";
+}
