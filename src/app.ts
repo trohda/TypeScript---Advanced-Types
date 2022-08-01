5e59416e6ce5541e859c4f74d24726294d81eab5
Tomasz Rohda <tomaszrohda@wp.pl>
2022-08-01 11:47:41 +0200

    Index Properties

--- src/app.ts
+++ src/app.ts
@@ -126,3 +126,12 @@ const userInputElement = document.getElementById("user-input");
 if (userInputElement) {
   (userInputElement as HTMLInputElement).value = "Hi there!";
 }
+
+interface ErrorContainer {
+  [prop: string]: string;
+}
+
+const errorBag: ErrorContainer = {
+  email: "Not a valid email!",
+  userName: "Must start with a capital character!",
+};
